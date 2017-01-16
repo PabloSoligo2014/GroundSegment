2280ddf5843ebbbd6c6cb7390a4a70541eab8104
Pablo Soligo <psoligo@gmail.com>
2017-01-15 22:44:54 -0300

    getPass en Sitio y PassTestScripts

--- GroundSegment/GroundSegment/models/Pasada.py
+++ GroundSegment/GroundSegment/models/Pasada.py
@@ -5,18 +5,22 @@ Created on Sep 2, 2016
 '''
 
 from django.db import models
+import ephem
 from GroundSegment.models.Satellite import Satellite
+from GroundSegment.models.Tle import Tle
 from GroundSegment.models.Sitio import Sitio
 
 class Pasada(models.Model):
-    name = models.CharField(max_length=128, unique=True)
     satellite   = models.ForeignKey(Satellite, related_name='pasadas')
-    sitio = models.ForeignKey(Sitio,related_name='pasadas')
-    startTime = models.DateTimeField(auto_now_add=True)
-    stopTime = models.DateTimeField(auto_now_add=True)
+    tle         = models.ForeignKey(Tle, related_name='pasadas', null=True)
+    sitio       = models.ForeignKey(Sitio,related_name='pasadas')
+    startTime   = models.DateTimeField(auto_now_add=True)
+    stopTime    = models.DateTimeField(auto_now_add=True)
+    duration    = models.FloatField('Duracion', help_text='[minutos]', default=0.0)
 
     def __str__(self):
         return self.name
+    
     
     
     
--- GroundSegment/GroundSegment/models/Sitio.py
+++ GroundSegment/GroundSegment/models/Sitio.py
@@ -4,14 +4,68 @@ Created on Sep 5, 2016
 @author: ubuntumate
 '''
 from django.db import models
-#from GroundSegment.models.Satellite import Satellite
+import ephem
+from GroundSegment.models.Satellite import Satellite
+from datetime import datetime
+from GroundSegment.Utils.UtilsFunctions import *
 #from django.contrib.admin.utils import help_text_for_field
 
 class Sitio(models.Model):
-    name = models.CharField("Nombre del Sitio", max_length=24, unique=True)
-    lat  = models.FloatField('Latitud', help_text='[Dato en Grados Decimales]', unique=True)
-    lon = models.FloatField('Longitud', help_text='[Grados Decimales]', unique=True)
-    h = models.FloatField('altura', help_text='[metros]', unique=True)
+    name     = models.CharField("Nombre del Sitio", max_length=24, unique=True)
+    lat      = models.FloatField('Latitud', help_text='[Dato en Grados Decimales]')
+    lon      = models.FloatField('Longitud', help_text='[Grados Decimales]')
+    h        = models.FloatField('Altura', help_text='[metros]')
+    maskElev = models.FloatField('Mascara de Elevacion', help_text='[grados]', default=0.0)
 
     def __str__(self):
         return self.name
+    
+    def getPass(self,satcode,idate):
+        from GroundSegment.models.Pasada import Pasada
+        '''
+        Sitio
+        '''
+        print("---------------PROXIMA PASADA----------------")
+        
+#        site=Sitio.objects.get(name='ETC')
+        
+        obs            = ephem.Observer()
+        obs.lon        = self.lon
+        obs.lat        = self.lat
+        obs.elevation  = self.h
+        obs.horizon    = self.maskElev
+        obs.date       = ephem.Date(idate)
+       
+        '''
+        Satelite
+        '''
+        
+        satelite = Satellite.objects.get(code=satcode)
+        print("NORAD ID = %s" % (satelite.noradId))
+        
+#         tle0 = satelite.getLastTLE()
+#         print(tle0.getLine1())
+               
+        line1 = satelite.code
+        line2="1 99937U          19245.00000000 -.00000688  00000-0 -74340-4 0 00007"
+        line3 = "2 99937 097.7872 318.3601 0010609 271.6129 088.3852 14.88384890000013"
+        sat0 = ephem.readtle(line1, line2, line3)
+        info = obs.next_pass(sat0)
+#         FS2017.compute(site)
+#         print(FS2017.alt,FS2017.eclipsed)
+        print("Rise time: %s Set Time: %s Duration [m]: %f" % (info[0], info[4],(info[4]-info[0])*1440))
+        
+        """
+        Registro de la pasada
+        """
+        pasada0             = Pasada()
+        pasada0.satellite   = satelite
+        pasada0.sitio       = self
+        pasada0.startTime   = datetime_from_time(info[0])
+        pasada0.stopTime    = datetime_from_time(info[4])
+        pasada0.duration    = (info[4]-info[0])*1440
+        
+        pasada0.save()
+        
+        
+        return None
